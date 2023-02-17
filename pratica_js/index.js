a8b02e8edb518153418b12a2fbd2039d372a9d6d
gabfm <gabriel1233217@gmail.com>
2023-02-17 11:41:18 -0300

    First commit

---
+++ pratica_js/index.js
@@ -0,0 +1,25 @@
+console.log("Eu funciono!")
+console.log ("Arquivo de script externo")
+
+let meuNumero = 10;
+let outroNumero = 15;
+console.log(meuNumero)
+meuNumero = 20
+console.log(meuNumero)
+
+console.log(meuNumero + outroNumero);
+
+console.log(`Meu número é: ${meuNumero + outroNumero}`);
+
+let meuPequenoTexto = "Estou treinando javascript"
+
+let variavel1
+let variavel2
+
+console.log(variavel1 + variavel2)
+
+let cpf1 = 036056011-99
+
+let cpf2 = "036056011-99"
+
+console.log( )
